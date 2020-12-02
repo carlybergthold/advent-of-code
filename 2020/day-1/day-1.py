490632f25a9340dc836d9240c66a19ff7eab38a4
Carly Bergthold <carlybergthold@gmail.com>
2020-12-02 11:57:13 -0600

    part two

--- 2020/day-1/day-1.py
+++ 2020/day-1/day-1.py
@@ -1,4 +1,13 @@
+# Part One
 for x in numbers:
     for y in numbers:
       if x + y == 2020:
         print(x, y)
+
+
+# Part Two
+for x in numbers:
+    for y in numbers:
+      for z in numbers:
+        if x + y + z == 2020:
+          print(x, y, z)
