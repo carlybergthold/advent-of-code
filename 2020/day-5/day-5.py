c8c45f2c8e46a321176e35a86a22e358a382b2cf
Carly Bergthold <carlybergthold@gmail.com>
2020-12-08 20:07:54 -0600

    day 6

--- 2020/day-5/day-5.py
+++ 2020/day-5/day-5.py
@@ -1,7 +1,46 @@
-import os
+import math
 
-slope = []
+boardingPasses = []
 with open('/Users/Carly/workspace/personal/advent-of-code/2020/day-5/input.txt', 'r') as f:
-    slope = [(i) for i in f.read().splitlines()]
+    boardingPasses = [(i) for i in f.read().splitlines()]
 
-print(slope)
+
+boardingPass = "FBFBBFFRLR"
+
+for index, char in enumerate(boardingPass):
+    print(index, char)
+
+    if index == 0:
+        frontHalf = True if boardingPass[0] == "F" else False
+        possibleRows = [0,63] if frontHalf else [64,127]
+
+    if 1 <= index <= 6:
+        frontHalf = True if char[index] == "F" else False
+        half = (possibleRows[1] - possibleRows[0]) / 2
+
+        if frontHalf:
+            possibleRows = [possibleRows[0], math.floor(half)]
+        else:
+            possibleRows = [math.ceil(half), possibleRows[1]]
+    print(possibleRows)
+
+    # leftHalf = True if boardingPass[7] == "L" else False
+    # possibleSeats = [0,3] if leftHalf else [4,7]
+
+    # leftHalf = True if boardingPass[8] == "L" else False
+    # if leftHalf:
+    #     possibleSeats = [0,1] if possibleSeats == [0,3] else [4,5]
+    # else:
+    #     possibleSeats = [4,5] if possibleSeats == [4,7] else [6,7]
+
+    # leftHalf = True if boardingPass[9] == "L" else False
+    # seat = possibleSeats[0] if leftHalf else possibleSeats[1]
+    # print(seat)
+
+    # uniqueId = row * 8 + seat
+    # print(uniqueId)
+
+
+
+
+
--- 2020/day-6/day-6.py
+++ 2020/day-6/day-6.py
@@ -1,7 +1,20 @@
-import os
-
-slope = []
+groups = []
 with open('/Users/Carly/workspace/personal/advent-of-code/2020/day-6/input.txt', 'r') as f:
-    slope = [(i) for i in f.read().splitlines()]
+    lines = [(i) for i in f.read().splitlines()]
 
-print(slope)
+group_count = 0
+groups.append([])
+for idx, line in enumerate(lines):
+    if line:
+        groups[group_count].append(line)
+    else:
+        group_count+=1
+        groups.append([])
+
+for idx, each in enumerate(groups):
+    groups[idx] = ''.join(''.join(each))
+
+yesCount = 0
+
+for group in groups:
+    yesCount += len(set(group))
