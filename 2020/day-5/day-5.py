55cb49aa97c654258709f41003f82667518edf66
Carly Bergthold <carlybergthold@gmail.com>
2020-12-25 16:57:27 -0600

    day 5 ish

--- 2020/day-5/day-5.py
+++ 2020/day-5/day-5.py
@@ -1,45 +1,31 @@
-import math
-
 boardingPasses = []
 with open('/Users/Carly/workspace/personal/advent-of-code/2020/day-5/input.txt', 'r') as f:
     boardingPasses = [(i) for i in f.read().splitlines()]
 
+totals = [128, 64, 32, 16, 8, 4, 2, 8, 4, 2]
+rows = [0,127]
+seats = [0,7]
+highestId = 0
 
-boardingPass = "FBFBBFFRLR"
+for boardingPass in boardingPasses:
+    for index, char in enumerate(boardingPass):
+        totalSeatsLeft = totals[index]
+        half = totalSeatsLeft / 2
 
-for index, char in enumerate(boardingPass):
-    print(index, char)
-
-    if index == 0:
-        frontHalf = True if boardingPass[0] == "F" else False
-        possibleRows = [0,63] if frontHalf else [64,127]
-
-    if 1 <= index <= 6:
-        frontHalf = True if char[index] == "F" else False
-        half = (possibleRows[1] - possibleRows[0]) / 2
-
-        if frontHalf:
-            possibleRows = [possibleRows[0], math.floor(half)]
+        if boardingPass[index] == "F":
+            rows[1] = rows[0] + (half - 1)
+        elif boardingPass[index] == "B":
+            rows[0] = rows[1] - (half - 1)
+        elif boardingPass[index] == "L":
+            seats[1] = seats[0] + (half - 1)
         else:
-            possibleRows = [math.ceil(half), possibleRows[1]]
-    print(possibleRows)
+            seats[0] = seats[1] - (half - 1)
 
-    # leftHalf = True if boardingPass[7] == "L" else False
-    # possibleSeats = [0,3] if leftHalf else [4,7]
+    uniqueId = rows[0] * 8 + seats[0]
+    if uniqueId > highestId:
+        highestId = uniqueId
 
-    # leftHalf = True if boardingPass[8] == "L" else False
-    # if leftHalf:
-    #     possibleSeats = [0,1] if possibleSeats == [0,3] else [4,5]
-    # else:
-    #     possibleSeats = [4,5] if possibleSeats == [4,7] else [6,7]
-
-    # leftHalf = True if boardingPass[9] == "L" else False
-    # seat = possibleSeats[0] if leftHalf else possibleSeats[1]
-    # print(seat)
-
-    # uniqueId = row * 8 + seat
-    # print(uniqueId)
-
+print(highestId)
 
 
 
